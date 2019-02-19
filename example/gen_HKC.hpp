c78f2dceb3419106259c8c9dfca6fdef83cf5579
cypridina <darrin.schultz91@gmail.com>
2019-02-18 18:16:40 -0800

    added header

---
+++ example/gen_HKC.hpp
@@ -0,0 +1,10 @@
+#include<string>
+
+typedef struct Vars {
+  uint32_t k = 0;
+  uint32_t min_count = 0;
+  uint32_t max_count = 0;
+  std::string out_prefix;
+} Vars;
+
+Vars process_cl_args(int argc, char **argv);
